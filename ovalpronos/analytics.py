6680ebc10c16ba1d94f85ed7d199801b2d9ba98f
Amaury Armengaud <amaury.lemaurarmengaud@gmail.com>
2026-09-11 15:05:49 +0200

    fix(analytics): initialize posthog with Posthog() instance instead of module attributes

--- ovalpronos/analytics.py
+++ ovalpronos/analytics.py
@@ -1,15 +1,20 @@
 from django.conf import settings
 
-_POSTHOG_HOST = 'https://eu.i.posthog.com'
+_client = None
+
+
+def _get_client():
+    global _client
+    if _client is None and settings.POSTHOG_ENABLED and settings.POSTHOG_API_KEY:
+        from posthog import Posthog
+        _client = Posthog(settings.POSTHOG_API_KEY, host='https://eu.i.posthog.com')
+    return _client
 
 
 def capture(user_pk: int, event: str, properties: dict = None):
-    if not (settings.POSTHOG_ENABLED and settings.POSTHOG_API_KEY):
-        return
     try:
-        import posthog as _posthog
-        _posthog.project_api_key = settings.POSTHOG_API_KEY
-        _posthog.host = _POSTHOG_HOST
-        _posthog.capture(f'user_{user_pk}', event, properties or {})
+        client = _get_client()
+        if client:
+            client.capture(f'user_{user_pk}', event, properties or {})
     except Exception:
         pass
